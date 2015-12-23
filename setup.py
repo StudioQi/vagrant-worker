d57e27fc8bd96ffb7662ee079674c3abda0922db
Aurelien Bondis <abondis@pheromone.ca>
2015-12-23 11:43:13 -0500

    distribute worker.py as a binary

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ from distutils.core import setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-setup(name='vagrant_worker',
-      version='0.5.2',
-      description='Service to call vagrant actions',
-      author='Pierre Paul Lefebvre',
-      author_email='info@pierre-paul.com',
-      install_requires=required,
-      url='https://jeto.io',
-      packages=['vagrant_worker'],
+setup(
+    name='vagrant_worker',
+    version='0.5.2',
+    description='Service to call vagrant actions',
+    author='Pierre Paul Lefebvre',
+    author_email='info@pierre-paul.com',
+    install_requires=required,
+    url='https://jeto.io',
+    packages=['vagrant_worker'],
+    data_files=[
+        ('bin', ['worker.py']),
+    ],
 )
